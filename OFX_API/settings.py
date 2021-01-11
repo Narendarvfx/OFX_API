28b8239d69901317e056a6eda0f7816406517040
Narendar Reddy <narendhar.c.c@gmail.com>
2021-01-11 20:01:58 +0530

    added Mysql config

--- OFX_API/settings.py
+++ OFX_API/settings.py
@@ -153,12 +153,34 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    import MySQLdb
+
+    connection = MySQLdb.connect(host='127.0.0.1',
+                                 port=3306,
+                                 user='ofx_data_admin',
+                                 passwd='data_admin@1234',
+                                 )
+    cur = connection.cursor()
+    cur.execute('CREATE DATABASE IF NOT EXISTS mist_erp;')
+    connection.close()
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'ofx_api',
+            'USER': 'ofx_data_admin',
+            'PASSWORD': 'data_admin@1234',
+            'HOST': '127.0.0.1',
+            'PORT': '3306'
+        },
+    }
 
 
 # Password validation
