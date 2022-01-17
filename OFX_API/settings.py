51084d33be4613aadac10fb228a40ca21632a916
Narendar Reddy <narendhar.c.c@gmail.com>
2022-01-17 15:27:56 +0530

    Update settings.py

--- OFX_API/settings.py
+++ OFX_API/settings.py
@@ -131,7 +131,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('192.168.5.20', '6379')],
+            "hosts": [('127.0.0.1', '6379')],
         },
     },
 }
