510563056d5dfe27570338cf958aa9f23f700c83
Narendar Reddy <narendhar.c.c@gmail.com>
2022-07-18 09:00:13 +0530

    Updated Production Sheet

--- hrm/urls.py
+++ hrm/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^api/hrm/employee/$', api.AllEmployeeDetail.as_view(), name='All Employee details'),
     url(r'^api/hrm/grades/', api.AllGrades.as_view(), name="All Grades"),
     url(r'api/hrm/teams/$', api.AllTeams.as_view(), name='All Team Details'),
+    url(r'api/hrm/leads/$', api.DeptLeads.as_view(), name='Get Dept Lead Details'),
     url(r'api/hrm/teams/(?P<id>\d+)/$', api.TeamById.as_view(), name='All Team Details'),
     url(r'api/hrm/permissions', api.AllPermissions.as_view(), name= 'Role Permissions'),
     url(r'api/hrm/role/role_permissions/(?P<role_id>\d+)/$', api.RolePermissions.as_view(), name=" Role Permissions by ID" )
