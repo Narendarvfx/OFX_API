d1fb68a24738f39083c56151f6b71d2165361cb5
Narendar Reddy <narendhar.c.c@gmail.com>
2022-10-05 21:40:40 +0530

    Update settings.py

--- OFX_API/settings.py
+++ OFX_API/settings.py
@@ -51,7 +51,7 @@ AUTHENTICATION_BACKENDS = (
 SECRET_KEY = '$qk=xmf1nr6xy)4-!w2g5!wh=)$e6^8@v^z%w@i8n44pjf5lg2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','*']
 
@@ -174,31 +174,25 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+
+if DEBUG:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-#
-# if DEBUG:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': 'ofx_api',
-#             'USER': 'root',
-#             'PASSWORD': 'VggGa6Kwq4bq',
-#             'HOST': '127.0.0.1',
-#             'PORT': '3306'
-#         },
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'ofx_api',
+            'USER': 'root',
+            'PASSWORD': 'VggGa6Kwq4bq',
+            'HOST': '127.0.0.1',
+            'PORT': '3306'
+        },
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
